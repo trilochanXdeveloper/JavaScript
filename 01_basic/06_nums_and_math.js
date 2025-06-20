7c2538c5d545d8449917e1a8040f111308385885
Trilochan Pradhan <trilochanpradhan2001@gmail.com>
2025-06-20 17:58:14 +0000

    Math And Number

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,24 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+
+//console.log(otherNumber.toPrecision(6));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//========================== Maths ============================
+
+//console.log(Math.random());
+//console.log((Math.random()*10)+1);
+//console.log(Math.floor((Math.random()*10)+1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
